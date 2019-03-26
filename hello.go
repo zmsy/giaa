dcc1828105da9909bb6cb3dd39c4d1344091c3a3
Zach Morrissey <zachmor05@gmail.com>
2019-03-25 23:12:37 -0700

    Adding basic main for golang api

---
+++ hello.go
@@ -0,0 +1,19 @@
+/*
+ * Main server loop.
+ */
+
+package main
+
+import (
+	//  "encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// our main function
+func main() {
+	router := mux.NewRouter()
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
