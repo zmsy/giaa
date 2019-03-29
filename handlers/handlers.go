743f99e7c062bb28a160cb7a47f76be5393fa1d5
Zach Morrissey <zachmor05@gmail.com>
2019-03-29 08:11:17 -0700

    Update handlers package and create datatypes

---
+++ handlers/handlers.go
@@ -0,0 +1,12 @@
+/*
+Handler functions for specific routes on the API.
+*/
+
+package handlers
+
+import "net/http"
+
+// returns a response for the index
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- main.go
+++ main.go
@@ -5,15 +5,16 @@
 package main
 
 import (
-	//  "encoding/json"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/zmsy/giaa/handlers"
 )
 
 // our main function
 func main() {
 	router := mux.NewRouter()
+	http.HandleFunc("/api/index", handlers.IndexHandler)
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
